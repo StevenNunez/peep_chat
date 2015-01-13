1e447cf6ac901553ee234334e80ac2f5d53931be
Steven Nunez <steven.nunez@gmail.com>
2015-01-12 22:05:28 -0500

    Move client out of web dir to speed up running; remove bad DB code

--- web/controllers/api/room_controller.ex
+++ web/controllers/api/room_controller.ex
@@ -3,9 +3,8 @@ defmodule PeepChat.RoomController do
   plug :action
 
   def index(conn, _params) do
-    rooms = PeepChat.Repo.all from r in PeepChat.Room, preload: [:messages]
-    # rooms = PeepChat.Room |>
-    #         PeepChat.Repo.all preload: [:messages]
+    rooms = PeepChat.Room |>
+            PeepChat.Repo.all preload: [:messages]
     json conn, %{rooms: rooms, messages: Enum.first(rooms).messages}
   end
 end
